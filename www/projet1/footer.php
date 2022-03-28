d29a7b019802a06433b5d6c3e5b078a86685e739
teaknicolas <75626128+teaknicolas@users.noreply.github.com>
2022-03-28 23:01:41 +0200

    les_pages_php

---
+++ www/projet1/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>projet1</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<footer>
+<div class="logos">
+    <a href="#"><img class="image1" src="insta.png" alt="logoinsta" width="20" height="20"></a>
+    <a href="#"><img class="image2" src="twitter.png" alt="logotwitter" width="20" height="20"></a>
+</div>
+</footer>
+</body>
+</html>
