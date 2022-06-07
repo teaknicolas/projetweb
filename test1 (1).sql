036853440a6aab6d6d3327ec68b3e2e95a5eaace
teaknicolas <75626128+teaknicolas@users.noreply.github.com>
2022-06-07 04:49:08 +0200

    rfgdgrg

--- test1 (1).sql
+++ test1 (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 21 avr. 2022 à 21:38
+-- Généré le :  mar. 07 juin 2022 à 02:48
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `catalogue` (
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `img` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `catalogue`
@@ -48,7 +48,18 @@ INSERT INTO `catalogue` (`id`, `categorie`, `produit`, `prix`, `qte`, `descripti
 (1, 'Processeur', 'AMD RYZEN 5 3600', '200€', '10', 'AMD RYZEN 5 3600, socket : AM4, 3.6/4.2 GHZ, 6 core/12 threads, TDP : 65W, chipset CM : A320, A520, B350, B450, B550, X370, X470, X570', 'img/amdryzen53600.jpg'),
 (2, 'Processeur', 'AMD RYZEN 7 3700x', '300€', '10', 'AMD RYZEN 7 3700x, socket : AM4, 3.6/4.4 GHZ, 8 core/16 threads, TDP : 65W, chipset CM : A320, A520, B350, B450, B550, X370, X470, X570', 'img/amdryzen73700x.jpg'),
 (4, 'Processeur', 'AMD RYZEN 3 3200g', '150€', '10', 'AMD RYZEN 3 3200g, socket : AM4, 3.6/4.4 GHZ, 4 core/8 threads, TDP : 65W, chipset CM : A320, A520, B350, B450, B550, X370, X470, X570', 'img/amdryzen33200g.jpg'),
-(6, 'Processeur', 'AMD RYZEN 5 5500', '204€', '10', 'AMD RYZEN 5 5500, socket : AM4, 3.2/4.2 GHZ, 6 core/12 threads, TDP : 65W, chipset CM : A320, A520, B350, B450, B550, X370, X470, X570', 'img/amdryzen55500.jpg');
+(6, 'Processeur', 'AMD RYZEN 5 5500', '204€', '10', 'AMD RYZEN 5 5500, socket : AM4, 3.2/4.2 GHZ, 6 core/12 threads, TDP : 65W, chipset CM : A320, A520, B350, B450, B550, X370, X470, X570', 'img/amdryzen55500.jpg'),
+(12, 'Processeur', 'INTEL CORE i5-10400', '200€', '10', '', 'img/intelcorei510400.jpg'),
+(9, 'Processeur', 'INTEL CORE i3-10100', '100€', '10', '', 'img/intelcorei310100.jpg'),
+(11, 'Processeur', 'INTEL CORE i3-12100', '160€', '10', '', 'img/intelcorei312100.jpg'),
+(13, 'Processeur', 'INTEL CORE i5-10400', '200€', '10', '', 'img/intelcorei510400.jpg'),
+(14, 'Processeur', 'INTEL CORE i5 10600k', '300€', '10', '', 'img/intelcorei510600k.jpg'),
+(15, 'Processeur', 'INTEL CORE i7 10700k', '400€', '10', '', 'img/intelcorei710700k.jpg'),
+(16, 'Souris', 'advancegta210', '10€', '10', '', 'img/advancegta210.jpg'),
+(17, 'Souris', 'AOC GM300B', '35€', '10', '', 'img/aocgm300b.jpg'),
+(18, 'Souris', 'Aorus M4', '50€', '10', '', 'img/aorusm4.jpg'),
+(19, 'Souris', 'Logitech G502', '60€', '10', '', 'img/logitechg502.jpg'),
+(20, 'Souris', 'Logitech G PRO', '150€', '10', '', 'img/logitechgpro.jpg');
 
 -- --------------------------------------------------------
 
@@ -86,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `panier` (
   `qte` int(11) NOT NULL,
   `id_produit` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `panier`
@@ -94,21 +105,14 @@ CREATE TABLE IF NOT EXISTS `panier` (
 
 INSERT INTO `panier` (`id`, `id_utilisateur`, `qte`, `id_produit`) VALUES
 (1, 72, 3, 2),
-(2, 72, 1, 1),
+(21, 72, 1, 15),
 (3, 75, 7, 2),
 (4, 72, 1, 2),
-(18, 72, 1, 1),
 (6, 75, 1, 2),
 (7, 75, 1, 2),
-(8, 72, 1, 2),
 (9, 75, 1, 2),
 (10, 72, 1, 6),
-(11, 72, 0, 1),
-(12, 72, 0, 4),
-(13, 72, 0, 4),
 (14, 72, 4, 4),
-(15, 72, 10, 4),
-(16, 72, 4, 4),
 (17, 75, 5, 6);
 
 -- --------------------------------------------------------
